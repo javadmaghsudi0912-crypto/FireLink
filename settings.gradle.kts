2c7d600090fc95c63bca00d1bb6576ac94cab226
javadmaghsudi0912-crypto <javadmaghsudi0912-crypto@users.noreply.github.com>
2026-08-29 21:33:20 +0330

    Use official Maven repositories in CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,18 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
     }
 }
+
 rootProject.name = "FireLine"
 include(":app")
